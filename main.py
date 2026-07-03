da976da10dbc0a00e416797bcf0b207819f6d05d
abdalhamedzeglam <abdalhamedzeglam@gmail.com>
2026-07-03 02:22:31 +0200

    Adding loss and accuracy scale

--- main.py
+++ main.py
@@ -3,21 +3,32 @@ import nnfs
 from nnfs.datasets import spiral_data
 from nn import layers
 from nn import activations
+from nn import losses
+from nn import metrics
 
 nnfs.init()
 
 x , y = spiral_data(100 , 3)
 
 Layer1 = layers.Layer_Dence(2 , 3)
-activations_layer1 = activations.ReLU_Activation()
+activation_layer1 = activations.ReLU_Activation()
 
 Layer_ouput = layers.Layer_Dence(3 , 3)
-activations_output = activations.Softmax_Activation()
+activation_output = activations.Softmax_Activation()
 
 Layer1.forward(x)
-activations_layer1.forward(Layer1.output)
+activation_layer1.forward(Layer1.output)
 
-Layer_ouput.forward(activations_layer1.output)
-activations_output.forward(Layer_ouput.output)
+Layer_ouput.forward(activation_layer1.output)
+activation_output.forward(Layer_ouput.output)
 
-print(activations_output.output[:5])
+print(activation_output.output[:5])
+
+loss_function = losses.Loss_Categorical_cross_entropy()
+loss = loss_function.calculate_loss(activation_output.output , y)
+
+metric = metrics.Accuracy()
+accuracy = metric.calculate(activation_output.output , y)
+
+print('loss:' , loss)
+print('accuracy:' , accuracy , '%')
